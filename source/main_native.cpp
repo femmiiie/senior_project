f3c915d364ae42c397ed3447af5990be605b5e90
Sandro Mocevic <sandro.mocev@gmail.com>
2026-01-23 22:20:47 -0500

    removed unneeded includes

--- source/main_native.cpp
+++ source/main_native.cpp
@@ -1,22 +1,14 @@
 #define WEBGPU_CPP_IMPLEMENTATION
 #include <webgpu/webgpu.hpp>
 
-#include <iostream>
-// #include <cassert>
-// #include <vector>
-// #include <cstring>
-
 #include "Application.h"
 #include "RenderContext/GLFWRenderContext/GLFWRenderContext.h"
 
 int main()
-{
+{ 
   GLFWRenderContext context = GLFWRenderContext();
 
-  if (!context.ok)
-  {
-    return 1;
-  }
+  if (!context.ok) { return 1; }
 
   Application app(context);
 
