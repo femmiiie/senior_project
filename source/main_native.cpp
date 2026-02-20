6be523c1efa708df970a1a59eea4375e12166117
Sandro Mocevic <sandro.mocev@gmail.com>
2026-02-20 13:16:05 -0500

    camera setup for native

--- source/main_native.cpp
+++ source/main_native.cpp
@@ -14,7 +14,21 @@ int main()
 	Renderer renderer({1920, 1080});
 
 	InputManager::Initialize(renderer.getWindow(), renderer.getUIContext());
+	
+	Camera camera;
+	camera.setScrollScaling(0.5f);
+	camera.getAspect_M() = 1920.0f / 1080.0f;
+	camera.deferUpdate();
 
+	InputManager::AddScrollCallback(
+		[&camera](double x, double y) { camera.OnScroll(x, y); }
+	);
+	InputManager::AddMouseButtonCallback(
+		GLFW_MOUSE_BUTTON_MIDDLE,
+		[&camera](int action, int mods) { camera.OnMouseButton(action, mods); }
+	);
+
+	renderer.SetCamera(&camera);
 
 	while (renderer.isRunning())
 	{
