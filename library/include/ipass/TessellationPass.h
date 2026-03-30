2466d0b41ddfb85afb2f9e58fdd1ae6623cee878
Sandro Mocevic <sandro.mocev@gmail.com>
2026-03-30 17:54:02 -0400

    api header files

---
+++ library/include/ipass/TessellationPass.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <webgpu/webgpu.hpp>
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include "PatchData.h"
+
+namespace ipass {
+
+class TessellationPass {
+private:
+    struct Impl; 
+    Impl* impl = nullptr;
+
+public:
+    TessellationPass(wgpu::Device device, const Config& config = {});
+    TessellationPass(wgpu::Device device, wgpu::Queue queue, const Config& config = {});
+    ~TessellationPass();
+
+    TessellationPass(const TessellationPass&)            = delete;
+    TessellationPass& operator=(const TessellationPass&) = delete;
+    TessellationPass(TessellationPass&&)                   noexcept;
+    TessellationPass& operator=(TessellationPass&&)        noexcept;
+
+    bool UploadPatches(const PatchData& data, wgpu::Buffer lod_buffer);
+    void Dispatch(wgpu::CommandEncoder& encoder);
+
+    wgpu::Buffer GetVertexBuffer() const;
+    uint32_t GetMaxVertexCount() const;
+    uint32_t GetPatchCount() const;
+
+};
+
+}
---
+++ library/include/ipass/ipass.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "PatchData.h"
+#include "LODPass.h"
+#include "TessellationPass.h"
+#include "Pipeline.h"
+#include "BVLoader.h"
