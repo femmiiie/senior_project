fca5808280c8fa768e9adbd09eb046841bdb3e02
Sandro Mocevic <sandro.mocev@gmail.com>
2026-04-01 11:59:31 -0400

    file handling and shader loading fixed on emscripten

--- source/Utils/File.h
+++ source/Utils/File.h
@@ -5,6 +5,7 @@
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#include <cstdio>
 #else
 #include <nfd.hpp>
 #endif
@@ -16,15 +17,50 @@ namespace utils
   //the callback signature is void(std::string path).
   using Callback = std::function<void(std::string)>;
 
+#ifdef __EMSCRIPTEN__
 
+  namespace detail {
+    inline Callback& PendingFileCallback() {
+      static Callback cb;
+      return cb;
+    }
+  }
 
   inline void OpenFile(const char* description, const char* extension, Callback callback)
   {
-  //web requires async access for file dialogs, impossible to have a shared implementation
-  // #ifdef __EMSCRIPTEN__
-    //todo add file opening for emscripten
+    (void)description; //remove unused parameter warning
+    detail::PendingFileCallback() = std::move(callback);
 
-  // #else
+    EM_ASM({
+      var accept = '.' + UTF8ToString($0);
+      var input = document.createElement('input');
+      input.type = 'file';
+      input.accept = accept;
+      input.onchange = function(e) {
+        var file = e.target.files[0];
+        if (!file) return;
+        var reader = new FileReader();
+        reader.onload = function() {
+          var data = new Uint8Array(reader.result);
+          var nameLen = lengthBytesUTF8(file.name) + 1;
+          var namePtr = _malloc(nameLen);
+          stringToUTF8(file.name, namePtr, nameLen);
+          var dataPtr = _malloc(data.length);
+          HEAPU8.set(data, dataPtr);
+          _emOpenFileComplete(namePtr, dataPtr, data.length);
+          _free(namePtr);
+          _free(dataPtr);
+        };
+        reader.readAsArrayBuffer(file);
+      };
+      input.click();
+    }, extension);
+  }
+
+#else
+
+  inline void OpenFile(const char* description, const char* extension, Callback callback)
+  {
     NFD::Guard guard;
     NFD::UniquePath path;
     nfdfilteritem_t filters[] = {{ description, extension }};
@@ -33,6 +69,22 @@ namespace utils
     {
       callback(std::string(path.get()));
     }
-  // #endif
   }
+
+#endif
 }
+
+#ifdef __EMSCRIPTEN__
+extern "C" inline EMSCRIPTEN_KEEPALIVE void emOpenFileComplete(const char* filename, const void* data, int dataLen)
+{
+  std::string path = std::string("/tmp/") + filename;
+  FILE* f = fopen(path.c_str(), "wb");
+  if (f)
+  {
+    fwrite(data, 1, (size_t)dataLen, f);
+    fclose(f);
+  }
+  auto& cb = utils::detail::PendingFileCallback();
+  if (cb) cb(path);
+}
+#endif
