ffe51656a70a3a88d4520eec5d6de73884d6efc3
Sandro Mocevic <sandro.mocev@gmail.com>
2026-04-06 13:44:00 -0400

    support max device buffer size, compute patch limits based on it

--- source/Pass/ComputePass/TessellatorPass.h
+++ source/Pass/ComputePass/TessellatorPass.h
@@ -12,10 +12,7 @@ class BVParser;
 class TessellatorPass : public ComputePass
 {
 public:
-  static constexpr uint32_t DEFAULT_PATCH_LIMIT = 64;
-  static_assert(DEFAULT_PATCH_LIMIT <= tess::MAX_PATCHES);
-
-  TessellatorPass(GPUContext& ctx, wgpu::Buffer ipass_levels_buf);
+  TessellatorPass(GPUContext& ctx, wgpu::Buffer ipass_levels_buf, uint32_t patchLimit);
   ~TessellatorPass();
 
   void LoadBV(const BVParser& parser);
@@ -29,6 +26,7 @@ public:
 private:
   Tessellator* tess = nullptr;
 
+  uint32_t maxPatchLimit = 64; // fallback if device limit query fails
   uint32_t num_quads    = 0;  // actual patch count after last LoadBV
   bool     initialized  = false;
 };
